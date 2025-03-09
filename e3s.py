28d9df415b352e09a6c103d861a2295147bb5433
Odrak <99odrak@gmail.com>
2025-03-09 03:59:23 +0100

    FnX

--- e3s.py
+++ e3s.py
@@ -1,3 +1,19 @@
 import ollama
 
+import googletrans
+
+import gtts 
+
+import whisper
+
+import warnings
+
+print(googletrans.Translator().translate('', '').text)
+
+gtts.gTTS('', lang='en').save('')
+
+print(whisper.load_model("base").transcribe('')['text'])
+
+warnings.filterwarnings("ignore")
+
 print(ollama.chat("", [{"role": "user", "content": ""}]['message']['content']))
